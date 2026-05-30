5065c9f6aa8256a7de572ef642904db1d53a2609
Harish5py <pyharish5@gmail.com>
2026-05-30 14:18:46 +0000

    Modified diabetes.py for checking the workflow build

--- diabetes.py
+++ diabetes.py
@@ -97,7 +97,7 @@ smoking_history_map = {
 
 
 # ---------------- APPLICATION UI ----------------
-st.title("🩺 Diabetes Prediction Ap for Harish")
+st.title("🩺 Diabetes Prediction App for Shabari")
 st.write("Enter the patient details below to predict diabetes risk.")
 
 with st.form("prediction_form"):
